a978a92d9a2e5daf3991be115b455518c73adb06
Denis Shevchenko <denis@youranswer.ch>
2020-12-02 13:12:06 +0100

    add server.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 module.exports = {
+  compress: false,
   i18n: {
-    locales: ['en', 'fr', 'nl'],
+    locales: ['en', 'de', 'fr', 'it'],
     defaultLocale: 'en',
+    localeDetection: false,
+    domains: [
+      {
+        domain: 'example.com',
+        defaultLocale: 'en'
+      },
+      {
+        domain: 'example.de',
+        defaultLocale: 'de'
+      },
+      {
+        domain: 'example.fr',
+        defaultLocale: 'fr'
+      },
+      {
+        domain: 'example.it',
+        defaultLocale: 'it'
+      },
+    ]
   },
 }
